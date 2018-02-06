f80a6031b12a907fcaca76e9305dbd2de00a2f6a
Elan Perach <elanperach@gmail.com>
2018-02-06 12:42:20 +0200

    fix local json file in mac

--- src/app/services/store.js
+++ src/app/services/store.js
@@ -1,13 +1,20 @@
 import electron from 'electron'
 import path from 'path'
-
-let userDataPath = (electron.app || electron.remote.app).getPath('userData');
-userDataPath = path.join(userDataPath, 'zen-wallet-db.json');
-
 import low from 'lowdb'
+import fs from 'fs'
 import FileSync from 'lowdb/adapters/FileSync'
 
-const adapter = new FileSync(userDataPath)
+const electronApp = (electron.app || electron.remote.app)
+const fileName = 'zen-wallet-db.json'
+const userDataPath = electronApp.getPath('userData')
+const appDataPath = electronApp.getPath('appData')
+const zenWalletPath = `${appDataPath}/zenwallet`
+
+if (!fs.existsSync(zenWalletPath)) { fs.mkdir(zenWalletPath) }
+
+const zenDataPath = path.join(userDataPath, fileName)
+
+const adapter = new FileSync(zenDataPath)
 const db = low(adapter)
 
 export default db
