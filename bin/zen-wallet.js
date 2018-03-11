a81cbffae4742ed688b84371f9a0268a30ecca25
Elan Perach <elanperach@gmail.com>
2018-03-11 15:34:09 +0200

    allowing user to wipe from zen-wallet command

--- bin/zen-wallet.js
+++ bin/zen-wallet.js
@@ -3,14 +3,18 @@
 const proc = require('child_process');
 const path = require('path');
 
-const wallet = proc.spawn("npm", ["start"], {
-    cwd: path.join(__dirname,'../')
-});
+let wallet;
+
+if (process.argv.indexOf("wipe") > -1) {
+  wallet = proc.spawn("npm", ["start", "wipe"], { cwd: path.join(__dirname,'../') });
+} else {
+  wallet = proc.spawn("npm", ["start"], { cwd: path.join(__dirname,'../') });
+}
 
 wallet.stdout.pipe(process.stdout);
 wallet.stderr.pipe(process.stderr);
 
 wallet.on('exit', function (code) {
-    console.log('child process exited with code ' + code.toString());
-    process.exit(code);
+  console.log('child process exited with code ' + code.toString());
+  process.exit(code);
 });
