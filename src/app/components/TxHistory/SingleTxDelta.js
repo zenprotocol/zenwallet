8d66a01bf68d173d76399ad2909bc05848e3af38
Elan Perach <elanperach@gmail.com>
2018-03-27 20:33:34 +0300

    Show ZENP asset name in tx history and normalize

--- src/app/components/TxHistory/SingleTxDelta.js
+++ src/app/components/TxHistory/SingleTxDelta.js
@@ -5,6 +5,8 @@ import {truncateString, normalizeTokens} from '../../../utils/helpers'
 
 import CopyableTableCell from '../UI/CopyableTableCell'
 
+const zenAsset = '0000000000000000000000000000000000000000000000000000000000000000'
+
 class SingleTxDelta extends Component {
   constructor() {
     super()
@@ -12,18 +14,26 @@ class SingleTxDelta extends Component {
   }
 
   render() {
-    const {tx} = this.props
+    const {asset, assetType, amount} = this.props.tx
+    let assetName, finalAmount
 
-    const truncatedHash = truncateString(tx.asset)
-    const amountClass = (tx.amount > 0 ? 'align-right light-green' : 'align-right red' )
-    const amountText = (tx.amount > 0 ? `+${tx.amount}` : tx.amount )
+    finalAmount = Math.abs(amount)
+    if (asset === zenAsset && assetType === zenAsset) {
+      assetName = 'ZENP'
+      finalAmount = normalizeTokens(finalAmount)  // / 100000000
+    } else {
+      finalAmount = finalAmount.toLocaleString()
+    }
+
+    const truncatedHash = truncateString(asset)
+    const amountClass = (finalAmount > 0 ? 'align-right green' : 'align-right red' )
 
     return (
       [
-        <CopyableTableCell string={tx.asset} />,
-        <CopyableTableCell string={tx.assetType} />,
+        <CopyableTableCell string={`${asset}:${assetType}`} />,
+        <td>{assetName}</td>,
         <td className={amountClass}>
-          {amountText}
+          {finalAmount}
         </td>
       ]
     )
