ee3664064af7e9d9e3883559cdfb9feefce0dbd0
goldylucks <adamgoldman7@gmail.com>
2018-04-09 13:18:33 +0300

    (UnlockWallet): refactor + submit on enter

--- app/components/UnlockWallet/UnlockWallet.js
+++ app/components/UnlockWallet/UnlockWallet.js
@@ -2,60 +2,56 @@ import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import { Link } from 'react-router-dom'
 import Flexbox from 'flexbox-react'
+import cx from 'classnames'
 
 @inject('loading')
 @observer
 class UnlockWallet extends Component {
   state = {
     password: '',
-    inputType: 'password'
+    hidePassword: true,
   }
 
   onClickTogglePasswordVisibility = () => {
-    const { inputType } = this.state
-    const newType = inputType === 'password' ? 'text' : 'password'
-    this.setState({ inputType: newType })
+    this.setState({ hidePassword: !this.state.hidePassword })
   }
 
-  onChange = (e) => {
-    const newValue = e.target.value.trim()
-    this.setState({ password: newValue })
+  onChange = (evt) => {
+    this.setState({ password: evt.target.value.trim() })
   }
 
-  onSubmitClicked = () => {
-    const { password } = this.state
-    this.props.loading.unlockWallet(password)
+  onSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.loading.unlockWallet(this.state.password)
   }
 
   render() {
-    const { password, inputType } = this.state
-
-    const passwordIconClassNames = inputType === 'password' ? 'fa fa-eye' : 'fa fa-eye-slash'
-
+    const { password, hidePassword } = this.state
     return (
       <Flexbox flexDirection="column" className="loading-container">
         <Flexbox flexDirection="column" className="center unlock-wallet">
           <h1>Unlock Your Wallet</h1>
           <p>Please enter your password</p>
+          <form onSubmit={this.onSubmit}>
+            <div className="input-group password">
+              <input
+                name="password"
+                type={hidePassword ? 'password' : 'text'}
+                value={password}
+                placeholder="Enter password"
+                className="input-group-field"
+                onChange={this.onChange}
+              />
+              <a className="input-group-label show-password" onClick={this.onClickTogglePasswordVisibility}>
+                <i className={cx('fa', { 'fa-eye': hidePassword, 'fa-eye-slash': !hidePassword })} />
+              </a>
+            </div>
 
-          <div className="input-group password">
-            <input
-              name="password"
-              type={inputType}
-              value={password}
-              placeholder="Enter password"
-              className="input-group-field"
-              onChange={this.onChange}
-            />
-            <a className="input-group-label show-password" onClick={this.onClickTogglePasswordVisibility}>
-              <i className={passwordIconClassNames} />
-            </a>
-          </div>
-
-          <button className="unlock" onClick={this.onSubmitClicked}>
-            <span>Unlock</span>
-            <i className="fa fa-unlock" />
-          </button>
+            <button className="unlock">
+              <span>Unlock</span>
+              <i className="fa fa-unlock" />
+            </button>
+          </form>
 
           <Link className="forgot-password" to="/import-or-create-wallet">
             Forgot your password? Import you wallet again
