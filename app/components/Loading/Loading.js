938d0883df1535792bcfa95e71d17768df847616
goldylucks <adamgoldman7@gmail.com>
2018-04-09 12:54:08 +0300

    refactor(Loading)

--- app/components/Loading/Loading.js
+++ app/components/Loading/Loading.js
@@ -1,20 +1,13 @@
 import path from 'path'
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import autobind from 'class-autobind'
 import Flexbox from 'flexbox-react'
 
 @inject('loading')
 @observer
 class Loading extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      loadingDotsClass: 'loading-dots display-none'
-    }
-
-    autobind(this)
+  state = {
+    loadingDotsClass: 'loading-dots display-none'
   }
 
   componentWillMount() {
@@ -22,15 +15,18 @@ class Loading extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
+    this.timeout = setTimeout(() => {
       this.setState({ loadingDotsClass: 'loading-dots' })
     }, 3650);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout)
   }
 
   render() {
     const { loadingDotsClass } = this.state
 
-    const logoSrc = path.join('assets/img/zen_logo_big_no_text.png')
     const loadingGif = path.join('assets/img/loading.gif')
     const zenLogoGif = path.join('assets/img/zen-animated-logo.gif')
 
@@ -38,7 +34,7 @@ class Loading extends Component {
       <Flexbox flexDirection="column" className="loading-container">
         <Flexbox flexDirection="column" className="center">
           <img className="zen-logo" src={zenLogoGif} alt="Zen Protocol Logo" />
-          <h1>5</h1>
+          <h1>Welcome to Zen Protocol</h1>
           <p>Loading, please wait</p>
           <img className={loadingDotsClass} src={loadingGif} alt="Loading Gif" />
         </Flexbox>
