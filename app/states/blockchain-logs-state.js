57faad1ca58ff45f54b920e07d452c6b8a817492
Elan Perach <elanperach@gmail.com>
2018-04-16 18:26:50 +0300

    blockchain logs optimization

--- app/states/blockchain-logs-state.js
+++ app/states/blockchain-logs-state.js
@@ -1,15 +1,23 @@
-import { observable } from 'mobx'
+import { observable, runInAction } from 'mobx'
 import { ipcRenderer } from 'electron'
 
 class BlockchainLogsState {
   @observable logs = []
+  pending = []
 
   constructor() {
     ipcRenderer.send('init-fetch-logs', null)
     ipcRenderer.on('blockchainLogs', (event, log) => {
       if (!log) { return }
-      this.logs = this.logs.concat(log).slice(-50)
+      this.pending.push(log)
     })
+
+    setInterval(() => {
+      runInAction(() => {
+        this.logs = this.logs.concat(this.pending).slice(-100)
+        this.pending = []
+      })
+    }, 1000)
   }
 }
 
