d382daa41ab048ae665de4b009467b8bc508ca50
Elan Perach <elanperach@gmail.com>
2018-05-30 13:22:14 +0300

    show asset name in tx history

--- app/components/TxHistory/SingleTxDelta.js
+++ app/components/TxHistory/SingleTxDelta.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import { normalizeTokens, isZenAsset } from '../../utils/helpers'
+import { normalizeTokens, isZenAsset, getAssetName } from '../../utils/helpers'
 import CopyableTableCell from '../UI/CopyableTableCell'
 
 class SingleTxDelta extends Component {
   render() {
     const { asset, amount } = this.props.tx
-    const amountClass = (amount > 0 ? 'align-right green' : 'align-right red')
+    const amountClass = (amount > 0 ? 'amount align-right green' : 'amount align-right red')
     const finalAmount = normalizeTokens(amount, isZenAsset(asset))
+    const assetName = getAssetName(asset)
 
     return (
       <React.Fragment>
         <CopyableTableCell string={asset} />
-        <td>{isZenAsset(asset) && 'ZENP'}</td>
+        <td>{assetName}</td>
         <td className={amountClass}>{finalAmount}</td>
       </React.Fragment>
 
