e82b14dc6a291fd1abfd1a1ec4ee1cfa1a0e65b3
Elan Perach <elanperach@gmail.com>
2018-06-30 19:07:29 +0300

    change server address to 11567

--- app/config/server-address.js
+++ app/config/server-address.js
@@ -1,6 +1,6 @@
 // @flow
 const localhost = 'http://127.0.0.1'
 
-export const getServerAddress = () => (process.env.ZEN_LOCAL_NET === 'L1' ? `${localhost}:36000` : `${localhost}:31567`)
+export const getServerAddress = () => (process.env.ZEN_LOCAL_NET === 'L1' ? `${localhost}:36000` : `${localhost}:11567`)
 
 export const getCrowdsaleServerAddress = () => (process.env.ZEN_LOCAL_NET === 'localhost' ? `${localhost}:3000` : 'https://www.zenprotocol.com')
