9ac5e4c7e1fab6098c7044aa5eff1e5cd9af4677
Adam Goldman <goldy@adamgoldman.me>
2018-07-09 14:39:40 +0300

    seed makeover WIP
    - hide secret phrase quiz inputs
    - option to toggle between password/text input states
    - refactor to use SeedInput component
    - add fadeIn animation to scss
    - add classname to UI button components

--- app/components/Icons/ToggleVisibilityIcon.js
+++ app/components/Icons/ToggleVisibilityIcon.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import cx from 'classnames'
 
-const ToggleVisibilityIcon = ({ shouldShow, ...rest }) => (
+const ToggleVisibilityIcon = ({ shouldShow, className, ...rest }) => (
   <FontAwesomeIcon
     icon={['far', shouldShow ? 'eye' : 'eye-slash']}
+    style={{ cursor: 'pointer' }}
+    className={cx(className, 'toggle-input-visibility-icon')}
     {...rest}
   />
 )
 
 ToggleVisibilityIcon.propTypes = {
   shouldShow: PropTypes.bool.isRequired,
+  className: PropTypes.string,
+}
+ToggleVisibilityIcon.defaultProps = {
+  className: '',
 }
 
 export default ToggleVisibilityIcon
