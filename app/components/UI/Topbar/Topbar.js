f162ddb04c93a47536bf17008dddb246f5ca2fad
Adam Goldman <goldy@adamgoldman.me>
2018-07-18 14:52:29 +0300

    (TopBar): remove "Total", refactor
    - use flow instead of proptypes
    - improve classname handling
    - cleanup unused code

--- app/components/UI/Topbar/Topbar.js
+++ app/components/UI/Topbar/Topbar.js
@@ -1,59 +1,28 @@
+// @flow
+
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import PropTypes from 'prop-types'
-import classnames from 'classnames'
+import cx from 'classnames'
 import Flexbox from 'flexbox-react'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
-@inject('balances', 'history')
+import BalancesState from '../../../states/balances-state'
+
+type Props = {
+  balances: BalancesState,
+  className?: string
+};
+
+@inject('balances')
 @observer
-class Header extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-  }
-
-  onBackClicked = (evt) => {
-    this.props.history.goBack()
-    evt.preventDefault()
-  }
-
-  onForwardClicked = (evt) => {
-    this.props.history.goForward()
-    evt.preventDefault()
-  }
-
-  renderBackButton() {
-    const history = this.props.history
-    const canGoBack = (history.index != 0)
-    const className = (canGoBack ? 'back-button active' : 'back-button')
-    return (
-      <a onClick={this.onBackClicked} className={className} disabled={!canGoBack}>
-        <FontAwesomeIcon icon={['far', 'angle-left']} />
-      </a>
-    )
-  }
-
-  renderForwardButton() {
-    const history = this.props.history
-    const canGoForward = (history.length - history.index) != 1
-    const className = (canGoForward ? 'forward-button active' : 'forward-button')
-
-    return (
-      <a onClick={this.onForwardClicked} className={className} disabled={!canGoForward}>
-        <FontAwesomeIcon icon={['far', 'angle-right']} />
-      </a>
-    )
-  }
-
+class Header extends Component<Props> {
   render() {
-    const { balances } = this.props
-    const className = classnames('header', this.props.className)
+    const { balances, className } = this.props
     return (
-      <Flexbox className={className} element="header" >
+      <Flexbox className={cx('header', className)} element="header" >
         <Flexbox flexGrow={1} />
         <div className="balance">
           <div className="balance-and-ticker">
-            <span className="total-balance">Total Balance</span>
+            <span className="total-balance">Balance</span>
             <span className="zen-symbol">ZP</span>
           </div>
           <div className="account-balance">{balances.zenDisplay}</div>
