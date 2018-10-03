89f04a9e91dcb519c0c768123dbfc71db571adc7
Cremba <david@brima.it>
2018-10-03 15:37:23 +0300

    Fixed Link for testnet
    Fixed Link for testnet

--- app/components/CopyableTableCell.js
+++ app/components/CopyableTableCell.js
@@ -2,14 +2,18 @@
 
 import React from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { inject } from 'mobx-react'
 
 import ExternalLink from '../components/ExternalLink'
 import { truncateString } from '../utils/helpers'
 import { isZenAsset } from '../utils/zenUtils'
+import NetworkStore from '../stores/networkStore'
+import { MAINNET } from '../constants'
 
 const { clipboard } = require('electron')
 
 type Props = {
+  networkStore: NetworkStore,
   string: string,
   istx?: boolean
 };
@@ -17,6 +21,7 @@ type Props = {
 type State = {
   copyText: string
 };
+@inject('networkStore')
 class CopyableTableCell extends React.Component<Props, State> {
   copyTimeout: TimeoutID
   state = {
@@ -33,6 +38,11 @@ class CopyableTableCell extends React.Component<Props, State> {
     }, 1250)
   }
 
+  get getLink() {
+    const { networkStore } = this.props
+    return networkStore.chain === MAINNET ? '' : 'testnet.'
+  }
+
   get formattedString() {
     const { string } = this.props
     return !isZenAsset(string) ? truncateString(string) : string
@@ -40,7 +50,7 @@ class CopyableTableCell extends React.Component<Props, State> {
   renderString() {
     const { string, istx } = this.props
     return istx ? (
-      <ExternalLink link={`https://zp.io/tx/${string}`}>
+      <ExternalLink link={`https://${this.getLink}zp.io/tx/${string}`}>
         {this.formattedString}
       </ExternalLink>
     ) : this.formattedString
