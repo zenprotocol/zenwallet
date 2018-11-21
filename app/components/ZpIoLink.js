dacd314755b3b88175ba08d21b9f12780e1a737d
Adam Goldman <goldy@adamgoldman.me>
2018-11-21 16:49:43 -0500

    add pagination to TxHistory
    - add react-table
    - delete SingleTxDelta
    - add reusable Component ZpIoLink
    - CopyableTableCell: refcator with ZpIoLink, extend to support ReactTable

---
+++ app/components/ZpIoLink.js
@@ -0,0 +1,31 @@
+// @flow
+
+import * as React from 'react'
+import { inject, observer } from 'mobx-react'
+
+import { MAINNET } from '../constants'
+import NetworkState from '../stores/networkStore'
+
+import ExternalLink from './ExternalLink'
+
+type Props = {
+  networkStore: NetworkState,
+  children: React.Node,
+  path: string
+};
+
+@inject('networkStore')
+@observer
+class ZpIoLink extends React.Component<Props> {
+  get base() {
+    return this.props.networkStore.chain === MAINNET
+      ? 'https://zp.io/'
+      : 'https://testnet.zp.io/'
+  }
+  render() {
+    const { path, children, ...restOfProps } = this.props
+    return <ExternalLink {...restOfProps} link={`${this.base}/${path}`}>{children}</ExternalLink>
+  }
+}
+
+export default ZpIoLink
