e90afb22f2704c8b278482075ac17e712140971d
Elan P <elanperach@gmail.com>
2018-12-31 15:41:02 +0200

    design touchups

--- app/components/Topbar/Topbar.js
+++ app/components/Topbar/Topbar.js
@@ -22,6 +22,9 @@ class Header extends Component<Props> {
     const { portfolioStore, className } = this.props
     return (
       <Flexbox className={cx('header', className)} element="header" >
+        <Flexbox className="release-candidate-header">
+          <p>Yesod - Release Candidate</p>
+        </Flexbox>
         <Flexbox flexGrow={1} />
         <div className="balance">
           <div className="balance-and-ticker">
