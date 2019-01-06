7fc185cea5555c5b46d72585a49bd4c49b2ea8f1
Cremba <david@brima.it>
2019-01-06 19:27:55 +0200

    RC Welcome Messages

--- app/onBoardingPages/WelcomeMessages/WelcomeMessages.js
+++ app/onBoardingPages/WelcomeMessages/WelcomeMessages.js
@@ -8,46 +8,23 @@ import OnBoardingLayout from '../Layout/Layout'
 
 const pageTexts = [
   {
-    img: 'image-source-blockchain.png',
-    title: 'What is Zen Protocol?',
-    bullets: [
-      'Zen Protocol (ZP) is a platform for peer-2-peer finance.',
-      'ZP wallet is a free, open-source client interface.',
-      'ZP allows you to interact with the blockchain',
-      'Be in full control of your keys & funds without relying on banks or exchanges.',
-    ],
-  },
-  {
-    img: 'image-wallet.png',
-    title: 'How does the ZP wallet work?',
-    description: 'We care about your safety ­– so please read the following:',
-    bullets: [
-      'When creating a wallet on Zen Protocol you are generating a cryptographic set of 24 words, a mnemonic passphrase (seed) and your public address.',
-      'If you send your public address to someone, they can send you Zen or other assets.',
-      'NEVER share your seed or wallet file– these allow anyone holding them complete control over assets in the wallet, including sending these assets.',
-      'The developers of this software have no access to your passphrase or seed. If you forget your password or lose your seed, we cannot recover it for you. Make sure to keep a copy of your seed in a secure place.',
-    ],
-  },
-  {
     img: 'image-security.png',
-    title: 'Zen Protocol Seed & Passwords',
-    description: 'We care about your safety ­– so please read the following:',
+    title: 'Welcome to the Zen Protocol Yesod Release Candidate',
+    description: 'Please take a minute to read and understand the following notes:',
     bullets: [
-      'Keep your seed and password safe.',
-      'Make backups of your seed.',
-      'Be aware of phishing websites or programs.',
+      'The release candidate is not the official Zen Protocol network and should be used with caution.',
+      'The release candidate is only meant to be used in order to review and understand the proposed changes to the protocol.',
+      'Following the Zen Protocol Authorized Protocol, there will be a vote to determine if the token holders wish to upgrade to the proposed release candidate.',
     ],
   },
   {
     img: 'image-attention.png',
-    title: 'Only YOU are in control',
+    title: 'Welcome to the Zen Protocol Yesod Release Candidate',
     description: 'We care about your safety ­– so please read the following:',
     bullets: [
-      'You are responsible for your own security.',
-      'No one can recover or change your private seed.',
-      'No one can recover your password.',
-      'No one can refund your transactions.',
-      'No one can freeze your accounts.',
+      'There is no “Import Wallet” option in this wallet - since it is not recommended to use your real seed with the release candidate.',
+      'To receive ZP tokens to try out the release candidate send an email to info@zenprotocol.com with your address and the title “Please send me some ZP RC tokens”',
+      'Click here to learn more about the release candidate',
     ],
   },
 ]
@@ -58,7 +35,7 @@ class WelcomeMessages extends Component {
   }
   onNextClicked = () => {
     const nextPage = this.state.currentPage + 1
-    if (nextPage === 4) {
+    if (nextPage === 2) {
       history.push(routes.IMPORT_OR_CREATE_WALLET)
     } else {
       this.setState({ currentPage: nextPage })
@@ -97,9 +74,6 @@ class WelcomeMessages extends Component {
             <ul>{bulletPointes}</ul>
           </Flexbox>
 
-          <Flexbox className="bullet-image" flexGrow={1} justifyContent="flex-end">
-            <img alt="Page" className={`page-${currentPage}`} src={`${IMG_BASE}/${pageDetails.img}`} />
-          </Flexbox>
 
         </Flexbox>
 
@@ -110,7 +84,7 @@ class WelcomeMessages extends Component {
           <Flexbox flexGrow={2} />
           <Flexbox flexGrow={1} justifyContent="flex-end" flexDirection="row">
             {this.renderBackButton()}
-            <button className="button-on-right" onClick={this.onNextClicked} >Next</button>
+            <button className="button-on-right" onClick={this.onNextClicked} >I Understand</button>
           </Flexbox>
         </Flexbox>
 
