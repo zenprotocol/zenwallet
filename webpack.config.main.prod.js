6b517ee3dc7cd60c30409dd68885d601f88d3bb1
David S. Brima <david@brima.it>
2019-05-05 14:24:46 +0300

    main prod

--- webpack.config.main.prod.js
+++ webpack.config.main.prod.js
@@ -4,7 +4,6 @@
 
 import webpack from 'webpack'
 import merge from 'webpack-merge'
-import UglifyJSPlugin from 'uglifyjs-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 import baseConfig from './webpack.config.base'
